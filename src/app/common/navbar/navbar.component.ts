51fc50426d909e7a5f4ec417d187a797fae4ba8b
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-03-24 23:32:59 +0100

    feat: add NavbarComponent with HTML, CSS, and test files for navigation functionality

---
+++ src/app/common/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Importez Router pour la redirection
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class NavbarComponent implements OnInit {
+  fullName: any = localStorage.getItem('userName');
+
+ // constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  // Méthode pour déconnecter l'utilisateur
+  logout(): void {
+   // this.authService.logout(); // Appelez la méthode logout du service
+  }
+}
